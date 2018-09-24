3876fa542261b538248a59188ec3ecac08c5202b
lein <lein.davir@gmail.com>
2018-09-24 15:30:37 -0500

    modified controller movie after validations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,45 +5,50 @@ class MoviesController < ApplicationController
     flash[:error] = 'The url you tried to retrieved was not found!!'
     redirect_to :root
   end
-    def index
-     @movies = Movie.order_movies
-     #render json: @movies
+  def index
+    @movies = Movie.order_movies
+    #render json: @movies
+  end
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+  def new
+    @movie = Movie.new 
+  end
 
-
-    end
-    def show
-      id = params[:id] # retrieve movie ID from URI route
-      @movie = Movie.find(id) # look up movie by unique ID
-      # will render app/views/movies/show.html.haml by default
-    end
-    def new
-      @movie = Movie.new 
-    end
-    def create
-      params.require(:movie)
-      permitted = params[:movie].permit(:title,:rating,:release_date)
-      @movie = Movie.create!(permitted)
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
       flash[:notice] = "#{@movie.title} was successfully created."
-      redirect_to movie_path(@movie)
+      redirect_to movies_path
+    else
+      render 'new' # note, 'new' template can access @movie's field values!
     end
-    def edit
-      @movie = Movie.find params[:id]
-    end
-    def update
-      @movie = Movie.find params[:id]
-      permitted = params[:movie].permit(:title,:rating,:release_date)
-      @movie.update_attributes!(permitted)
-       
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    if @movie.update_attributes(movie_params)
       flash[:notice] = "#{@movie.title} was successfully updated."
-      respond_to do |client_wants|
-        client_wants.html {  redirect_to movie_path(@movie)  } # as before
-        client_wants.xml { render :xml => @movie.to_xml }
-      end
-   end
-   def destroy
+      redirect_to movie_path(@movie)
+    else
+      render 'edit' # note, 'edit' template can access @movie's field values!
+    end
+  end
+
+  def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-    end
+  end
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date, :description)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   scope :order_movies, ->  {order(title: :asc)}
-  
+
   def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #  shortcut: array of strings
   validates :title, :presence => true
   validates :release_date, :presence => true
